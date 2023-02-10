f642a246fe44fe131ff4e43ddcc7a5e8cee7b6a0
Stepan <sud_pravo@bk.ru>
2023-02-10 21:13:22 +0700

    Добавлен файл task004 с задачей 8

---
+++ Task/task004/Program.cs
@@ -0,0 +1,18 @@
+﻿
+        int n = 8;
+        
+        int index;
+        
+        index = 1;
+        while (index <= n)
+        {
+            if (index % 2 == 0)
+            {
+                Console.WriteLine(index);
+            }
+            index = index + 1;
+        }
+    
+  
+    
+
